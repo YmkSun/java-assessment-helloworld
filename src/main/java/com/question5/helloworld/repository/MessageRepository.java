6aadc4c353930af8df745dfdb08740f9ae04e831
ymksun <ymksun@localhost>
2025-09-04 02:26:56 +0700

    update missing annotation

--- src/main/java/com/question5/helloworld/repository/MessageRepository.java
+++ src/main/java/com/question5/helloworld/repository/MessageRepository.java
@@ -1,9 +1,11 @@
 package com.question5.helloworld.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.question5.helloworld.entity.Message;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 }
